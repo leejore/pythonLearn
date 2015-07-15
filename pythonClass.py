393c01737b8091517033f8db0226fb03a98cb582
leejore <leejore2079@163.com>
2015-07-15 22:14:31 +0800

    add private meber

--- pythonClass.py
+++ pythonClass.py
@@ -2,6 +2,7 @@ class FLY:
 	price=123		#static member
 	def __init__(self):#constuctor
 		self.directon="to beijing"#class member
+		self.__name="leejore"#class member
 		time="one hour"#local member
 
 
@@ -9,5 +10,7 @@ if  __name__=="__main__":
 	print(FLY.price)
 	fly=FLY()#ok
 	print(fly.directon)#ok
-	print(fly.time)#error
+	#print(fly.time)#error
+	#print(fly.__name)#error  it is private
+	print(fly._FLY__name)#ok this is always for test
       
