95587887abea072960e5af5021bd77f30ae84b5e
leejore <leejore2079@163.com>
2015-07-16 00:14:15 +0800

    python inMember

---
+++ inMemberFunction.py
@@ -0,0 +1,55 @@
+class room(object):
+	personCount=0
+	
+			
+	def __init__(self,name):
+		def sayHi():
+			print("hello,I am ",name)
+		self.name=name
+		room.personCount=room.personCount+1
+		print("inition your name is :",name," used  room :" ,room.personCount)
+		sayHi()
+		
+	
+	def __del__(self):
+		print("bye ",self.name)
+		room.personCount=room.personCount-1
+		if(room.personCount==0):
+			print("the romm is empty")
+		else:
+			print("the room still have ",room.personCount)
+
+	
+	def __setitem__(self,key,value):
+		
+		print((key,value))
+
+	
+	def __getitem__(self,key):
+		print("i am finding")
+
+	
+	def __eq__(self,another):
+		print("compare two instance ")
+		return False
+
+
+	def __delitem__(self,key):
+		print("del is called")
+			
+
+	
+	
+
+	
+
+if(__name__=="__main__"):
+	room1=room("leejore")
+	room2=room("lucy")
+	room2['10']='100'
+	room2['10']
+	del room1["pp"]
+	if(room2==room1):
+		print("they are equal")
+
+		
