71be932d1ffdf172da6a2d0ab6715d551c793983
leejore <leejore2079@163.com>
2015-07-15 22:28:04 +0800

    over member test

--- pythonClass.py
+++ pythonClass.py
@@ -12,5 +12,10 @@ if  __name__=="__main__":
 	print(fly.directon)#ok
 	#print(fly.time)#error
 	#print(fly.__name)#error  it is private
+	fly.distance="1000 miles"
+	print(fly.distance)#ok instance add member
 	print(fly._FLY__name)#ok this is always for test
+
+	anotherfly=FLY()
+	print(anotherfly.distance)#error distance can only ask in fly
       
