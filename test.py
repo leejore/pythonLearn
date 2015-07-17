c43d0c67f55b06fe720c1370294872137fae8c26
leejore <leejore2079@163.com>
2015-07-18 00:40:36 +0800

    get set

---
+++ test.py
@@ -0,0 +1,32 @@
+
+
+
+class lee1(object):
+	def __get__(self,obj,type=None):
+		print("__get__ :")
+		return self.data
+	
+
+	def __set__(self,obj,val):
+		print("__set__:")
+		self.data=val
+
+	def __delete__ (self,obj):
+		print("__delete__:")
+		del self.data
+
+
+
+class loo(object):
+	th=lee1()	
+	fuck=90
+
+jiji=loo()
+jiji.th="kkkkkkkk"
+print(jiji.th)	
+del jiji.th
+
+jiji.th="1231321"
+print(jiji.th)	
+
+            
